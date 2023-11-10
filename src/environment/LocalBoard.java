1c66715da2b620ec5bc0ab5da039e7c0f17358dc
P10r10 <asantiago@keemail.me>
2023-11-10 15:53:44 +0000

    snake goes to goal

--- src/environment/LocalBoard.java
+++ src/environment/LocalBoard.java
@@ -15,42 +15,42 @@ import game.Server;
 import game.Snake;
 import game.AutomaticSnake;
 
-/** Class representing the state of a game running locally
- * 
- * @author luismota
+/**
+ * Class representing the state of a game running locally
  *
+ * @author luismota
  */
 public class LocalBoard extends Board {
-	
-	private static final int NUM_SNAKES = 10; // default = 2
-	private static final int NUM_OBSTACLES = 0; // default = 25
-	private static final int NUM_SIMULTANEOUS_MOVING_OBSTACLES = 3;
 
-	public LocalBoard() {
-		
-		for (int i = 0; i < NUM_SNAKES; i++) {
-			AutomaticSnake snake = new AutomaticSnake(i, this);
-			snakes.add(snake);
-		}
-		addObstacles( NUM_OBSTACLES);
-		Goal goal = addGoal();
-		System.err.println("All elements placed");
-	}
+    private static final int NUM_SNAKES = 1; // default = 2
+    private static final int NUM_OBSTACLES = 0; // default = 25
+    private static final int NUM_SIMULTANEOUS_MOVING_OBSTACLES = 3;
 
-	public void init() {
-		for(Snake s : snakes)
-			s.start();
-		// TODO: launch other threads
-		setChanged();
-	}
+    public LocalBoard() {
 
-	@Override
-	public void handleKeyPress(int keyCode) {
-		// do nothing... No keys relevant in local game
-	}
+        for (int i = 0; i < NUM_SNAKES; i++) {
+            AutomaticSnake snake = new AutomaticSnake(i, this);
+            snakes.add(snake);
+        }
+        addObstacles(NUM_OBSTACLES);
+        Goal goal = addGoal();
+        System.err.println("All elements placed");
+    }
 
-	@Override
-	public void handleKeyRelease() {
-		// do nothing... No keys relevant in local game
-	}
+    public void init() {
+        for (Snake s : snakes)
+            s.start();
+        // TODO: launch other threads
+        setChanged();
+    }
+
+    @Override
+    public void handleKeyPress(int keyCode) {
+        // do nothing... No keys relevant in local game
+    }
+
+    @Override
+    public void handleKeyRelease() {
+        // do nothing... No keys relevant in local game
+    }
 }
--- src/game/AutomaticSnake.java
+++ src/game/AutomaticSnake.java
@@ -17,6 +17,20 @@ public class AutomaticSnake extends Snake {
         super(id, board);
     }
 
+    private Cell pickCandidateCell() {
+        // distance between snake's head and goal
+        Double distToGoal = cells.getFirst().getPosition().distanceTo(getBoard().getGoalPosition());
+        Cell toReturn = null;
+        List<BoardPosition> neighbourPos = getBoard().getNeighboringPositions(cells.getFirst());
+        for (BoardPosition bp : neighbourPos) { // choose shortest distance to goal
+            Double candidate = bp.distanceTo(getBoard().getGoalPosition());
+            if (candidate < distToGoal) {
+                distToGoal = candidate;
+                toReturn = getBoard().getCell(bp);
+            }
+        }
+        return toReturn;
+    }
     @Override
     public void run() {
         doInitialPositioning();
@@ -29,20 +43,36 @@ public class AutomaticSnake extends Snake {
         }
         //TODO: automatic movement
 
+//        while (true) {
+//            try {
+//                Thread.sleep(Board.PLAYER_PLAY_INTERVAL);
+//                List<BoardPosition> neighbourPos = getBoard().getNeighboringPositions(this.cells.getFirst());
+//                Cell nextCell;
+//                do { // next position is random // MODIFY towards target after
+//                    BoardPosition nextPos = neighbourPos.get(new Random().nextInt(neighbourPos.size()));
+//                    nextCell = getBoard().getCell(nextPos);
+//                } while (nextCell.isOcupiedBySnake());
+//                move(nextCell);
+//
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+
         while (true) {
             try {
                 Thread.sleep(Board.PLAYER_PLAY_INTERVAL);
-                List<BoardPosition> neighbourPos = getBoard().getNeighboringPositions(this.cells.getFirst());
                 Cell nextCell;
-                do { // next position is random // MODIFY towards target after
-                    BoardPosition nextPos = neighbourPos.get(new Random().nextInt(neighbourPos.size()));
-                    nextCell = getBoard().getCell(nextPos);
+                do { // moves towards target
+                    nextCell = pickCandidateCell();
                 } while (nextCell.isOcupiedBySnake());
                 move(nextCell);
-
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
+
+
+
     }
 }
